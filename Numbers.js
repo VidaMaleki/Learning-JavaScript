1e4e6dff5df6776b3bacd5cd41781b9faa9dd563
Vida Maleki <Vida.Basmani@gmail.com>
2020-12-28 19:33:12 -0800

    making four files for 01/03/2021 test

---
+++ Numbers.js
@@ -0,0 +1,3 @@
+/* Arithmatic Operators:
+1. Add: +     2. Subtract: - 3. Multiply: *  4. Divide: /  5. Remainder: %  */
+
---
+++ strings.js
@@ -0,0 +1,19 @@
+/*String Concatenation: 
+Front space, back space, no space & middle space */
+/*Example:
+1. Inside a console.log() statement, concatenate the two strings 'Hello' and 'World'. */
+console.log('Hello' + 'World'); //print HelloWorld
+/*Properties:
+Length*/
+/* Example
+Use the .length property to log the number of characters in the following string to the console:
+'Teaching the world how to code' */
+console.log('Teaching the world how to code'.length);
+/* Methods:
+We call, or use by appending an instance with:
+- A period( the dot operator)
+- The name of the method
+- opening and closing parentheses*/
+/* Example:
+1. Use the .toUpperCase() method to log the string 'Codecademy' to the console in all capital letters.
+
