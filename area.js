217ca607c5e6b85fe1838b5f3109fe66d7c5561a
Vida Maleki <Vida.Basmani@gmail.com>
2021-01-08 21:45:40 -0800

    square area added to the file

--- area.js
+++ area.js
@@ -1,2 +1,5 @@
-const rectangleArea = (width, height ) => width * height
-module.exports = rectangleArea;
+const rectangleArea = (width, height ) => width * height;
+//const triangleArea = (base, height) => (base * height) /2
+//const area = (base, height) => triangleArea(base, height) * 0.5;
+const squareArea = (a) => a * a
+module.exports = { rectangleArea, squareArea }
--- area.test.js
+++ area.test.js
@@ -1,4 +1,10 @@
-const rectangleArea = require('./area')
+const { rectangleArea, squareArea } = require('./area')
 test('rectangleArea 2 * 3 equals 6', () => {
     expect(rectangleArea(2, 3)).toBe(6)
+})
+/*test('triangle area 3 * 4 / 2 equals 6', () => {
+    expect(triangleArea(3, 4)).toBe(6)
+})*/
+test('squareArea 2 * 2 equals 4', () => {
+    expect(squareArea(2)).toBe(4)
 });
