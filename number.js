1f09aeef15605c0c46d81e997586cb6d6f40fd06
Vida Maleki <Vida.Basmani@gmail.com>
2021-01-11 16:58:07 -0800

    file has been modified

--- number.js
+++ number.js
@@ -1,5 +1,9 @@
-const sum = (a, b) => a + b
-const mul = (a, b) => a * b
+function sum(a, b){
+    return a + b;
+}
+function mul(a, b){
+    return a * b;
+}
 const sub = (a, b) => a - b
 const div = (a, b) => a / b
 module.exports = { sum, mul, sub, div }
