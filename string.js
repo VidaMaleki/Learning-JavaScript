dbadd22135abbc5f694cee70888171bd9c98a560
Vida Maleki <Vida.Basmani@gmail.com>
2021-01-11 16:56:19 -0800

    strin concatination added to the file

--- string.js
+++ string.js
@@ -1,6 +1,6 @@
 //This unction is to making first letter Uppercase.
 const capitalizeName = (name) => {
-    return name.charAt(0).toUpperCase() + name.slice(1); 
+    return name.charAt(0).toUpperCase() + name.slice(1);
 };
 //This function is for lowercasing a word.
 const lowerCase = (name) => {
@@ -9,5 +9,15 @@ const lowerCase = (name) => {
 //his function is for counting length of a sentence
 const txt = (sentence) => {
     return sentence.length;
-};    
-module.exports = { capitalizeName, lowerCase, txt }
+};
+//using 
+const str = (name) => {
+    return 'I am learning coding by ' + name + ' help.';
+};
+
+function concatination(sentence1, sentence2) {
+    return sentence1.concat(sentence2);
+}
+
+
+module.exports = { capitalizeName, lowerCase, txt, str, concatination }
