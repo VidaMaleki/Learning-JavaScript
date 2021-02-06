e500e89689740f232856ae934dbf739fce125794
Vida Maleki <Vida.Basmani@gmail.com>
2021-02-06 15:23:09 -0800

    multiplying and dividing added to the file

--- example.js
+++ example.js
@@ -16,3 +16,22 @@ function exponent(base, power) {
     return answer;
 };
 console.log(exponent(3, 3))
+    // multiplying without * using while loop
+function mul(a, b) {
+    let c = a;
+    let i = 1;
+    while (i < b) {
+        i++;
+        c += a;
+    }
+    return c;
+}
+console.log(mul(2, 5));
+// dividing without / using for loop
+function divid(dividend, divisor) {
+    let quotient = 0;
+    for (let i = dividend; i > 0; i -= divisor) {
+        quotient++
+    }
+    return quotient;
+};
