7e297c72f704b63ede7df262d6e970d856e10c43
Vida Maleki <vida.basmani@gmail.com>
2021-07-16 14:49:01 -0700

    first commit for leetcode Array problems

--- sumOfTwo.js
+++
@@ -1,9 +0,0 @@
-nums = [2, 7, 11, 15];
-target = 9;
-for (let i = 0; i < nums.length(); i++) {
-    for (let j = 0; j < nums.length(); j++) {
-        if (i + j === target) {
-            console.log(nums[i], [j])
-        }
-    }
-}
