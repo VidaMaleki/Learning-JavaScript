4b8e6b30265fa051f0b175fdfcec9bb28fe2b90f
Vida Maleki <vida.basmani@gmail.com>
2021-07-31 01:03:58 -0700

    daily task for leetcode

--- leetCode/duplicateZeros.js
+++ leetCode/duplicateZeros.js
@@ -15,9 +15,9 @@ Explanation: After calling your function, the input array is modified to: [1,2,3
 */
 var duplicateZeros = function(arr) {
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == 0) {
-            arr.pop();
+        if (arr[i] === 0) {
             arr.splice(i++, 0, 0);
+            arr.pop();
         }
     }
 };
